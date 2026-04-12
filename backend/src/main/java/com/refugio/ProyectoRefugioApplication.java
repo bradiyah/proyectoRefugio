0988e328f4e5e0ef4d8bc2195752d12f5e43b325
bradiyah <htmtf111@gmail.com>
2026-04-12 14:15:13 +0200

    trying it

--- backend/src/main/java/com/refugio/ProyectoRefugioApplication.java
+++ backend/src/main/java/com/refugio/ProyectoRefugioApplication.java
@@ -1,3 +1,13 @@
+package com.refugio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ProyectoRefugioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoRefugioApplication.class, args);
+    }
 
 }
